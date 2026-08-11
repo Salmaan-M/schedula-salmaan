a32e729061c214fc8f4a0a3b2b4df32ca5750f2e
Salmaan-M <133296753+Salmaan-M@users.noreply.github.com>
2026-08-11 15:37:41 +0530

    updated es

--- src/doctor/doctor.module.ts
+++ src/doctor/doctor.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { DoctorController } from './doctor.controller';
 import { DoctorService } from './doctor.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { SchedulingModule } from '../scheduling/scheduling.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    SchedulingModule,
+  ],
   controllers: [DoctorController],
   providers: [DoctorService],
 })
